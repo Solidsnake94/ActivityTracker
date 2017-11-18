84a7117b3f5052a88c314d840fb2bb9cbf89c720
Pawel Smolinski <pawelsmolinski94@gmail.com>
2017-11-18 06:22:02 +0100

    Cleaning up the boilerplate code

--- ActivityTracker.Client/src/App.js
+++ ActivityTracker.Client/src/App.js
@@ -5,27 +5,13 @@ import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import ChatBox from './components/chat/ChatBox/ChatBox';
 import SideBarList from './components/side-bar/SideBarList/SideBarList';
 import Header from './components/header/Header';
-import { subscribeToTimer } from './api';
 
 class App extends React.Component {
 
   constructor(props) {
     super(props);
 
-    subscribeToTimer((timestamp) => {
-      this.setState({
-        timestamp: timestamp
-      });
-    });
-
   };
-
-  state = {
-    timestamp: 'no timestamp'
-  };
-
-
-
 
   render() {
     return (
@@ -40,19 +26,6 @@ class App extends React.Component {
           <p className="App-intro">
             To get started, edit <code>src/App.js</code> and save to reload.
           </p>
-
-          <div className="App-chat-container">
-            <Header />
-            <div style={{ display: 'flex', flexDirection: 'row' }}>
-              <SideBarList />
-              <ChatBox />
-            </div>
-
-            <div>
-              This is value of the timestamp: {this.state.timestamp}          
-            </div>
-
-          </div>
 
         </div>
       </MuiThemeProvider>
